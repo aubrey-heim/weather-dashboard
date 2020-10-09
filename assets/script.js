5b995d913d20b9719c0ed20506f5f13d70b5806c
aubrey-heim <aubrey.l.heim@gmail.com>
2020-10-09 16:05:00 -0700

    made API call for forecast weather

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,16 @@
 var lastSearched = JSON.parse(localStorage.getItem("searchedCities"));
 
 if(lastSearched){
-    searchedCitites = [lastSearched]
-    displayCity()
-    
+    searchedCitites = [lastSearched]   
 } else {
-    searchedCitites = []
+    searchedCitites = [ellensburg]
 }
 let city = searchedCitites[0]
 
 let currentQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=b6a20b6fa343f5f3b06524bb9d62b04b"
+let forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q="+ city + "&appid=b6a20b6fa343f5f3b06524bb9d62b04b"
+
+displayCity()
 
 
 
@@ -20,6 +21,15 @@ function displayCity(){
     }).then(function(response){
         console.log(response)
         })
+
+    $.ajax({
+        url: forecastQueryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response)
+        })
+    
+    
 }
 
 $("#search").on("click", function(event){
